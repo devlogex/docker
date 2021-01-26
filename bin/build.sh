2246b1f0bf332440f79bd104e962172a3758b0d2
devlogex <tannd.devlogex@gmail.com>
2021-01-26 13:35:52 +0000

    update docker build

--- bin/build.sh
+++ bin/build.sh
@@ -9,15 +9,15 @@ then
         cd $PATH_PROJECTS/$i && docker build -t $i .
     done
 else
-    cd $PATH_PROJECTS/docker/env/maven && sh build.sh
+    # cd $PATH_PROJECTS/docker/env/maven && sh build.sh
     cd $PATH_PROJECTS/docker/env/pw_env && sh build.sh
     cd $PATH_PROJECTS/docker/env/redis && sh build.sh
 
-    cd $PATH_PROJECTS/dbservice && docker build -t dbservice .
-    cd $PATH_PROJECTS/config-service && docker build -t configservice .
-    cd $PATH_PROJECTS/strategy && docker build -t strategyservice .
-    cd $PATH_PROJECTS/dev-service && docker build -t devservice .
-    cd $PATH_PROJECTS/action-service && docker build -t actionservice .
-    cd $PATH_PROJECTS/report-service && docker build -t reportservice .
+    cd $PATH_PROJECTS/dbservice && mvn clean package && docker build -t dbservice .
+    cd $PATH_PROJECTS/config-service && mvn clean package && docker build -t configservice .
+    cd $PATH_PROJECTS/strategy && mvn clean package && docker build -t strategyservice .
+    cd $PATH_PROJECTS/dev-service && mvn clean package && docker build -t devservice .
+    cd $PATH_PROJECTS/action-service && mvn clean package && docker build -t actionservice .
+    cd $PATH_PROJECTS/report-service && mvn clean package && docker build -t reportservice .
     cd $PATH_PROJECTS/ToolProductManagermentUI && docker build -t fe .
 fi
--- bin/tag.sh
+++ bin/tag.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+docker image rm devlogex/dbservice
+docker image rm configservice devlogex/configservice
+docker image rm strategyservice devlogex/strategyservice
+docker image rm devservice devlogex/devservice
+docker image rm actionservice devlogex/actionservice
+docker image rm reportservice devlogex/reportservice
+docker image rm fe devlogex/fe
+
 docker tag dbservice devlogex/dbservice
 docker tag configservice devlogex/configservice
 docker tag strategyservice devlogex/strategyservice
